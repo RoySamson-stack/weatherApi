43c8cc344c363963051bfd82ba9131ff5949683c
RoySamson-stack <rsam9009@gmail.com>
2021-12-27 21:09:49 +0300

    making the data read from the input box to be displayed ion the screen

--- app.js
+++ app.js
@@ -3,10 +3,21 @@ const express = require("express");
 const https = require("https");
 
 const app = express();
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=e29ded393ef674cd5e29020650bec2d2"
+  res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", function(req, res){
+  //getting text from the input user has written install body-parser
+
+  const query = req.body.cityName;
+  const apiKey = "e29ded393ef674cd5e29020650bec2d2"
+  const unit = "metric"
+  const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid=e"+ apiKey +"&units=" + unit;
 
   https.get(url, function (response) {
     console.log(response.statusCode);
@@ -15,7 +26,7 @@ app.get("/", function (req, res) {
       //converting to a javascript object
       const weatherData = JSON.parse(data)
       //getting data from the tree of JSON
-      const temp = weatherData.main.temp
+      const temp = weatherData[0].main.temp
       const weatherDescription = weatherData.weather[0].description
       const icon = weatherData.weather[0].icon
       const imageUrl = "http://openweathermap.org/img/wn/"+ icon +"@2x.png"
@@ -32,6 +43,7 @@ app.get("/", function (req, res) {
     })
   })
 })
+
 
 
 app.listen(3000, () => {
