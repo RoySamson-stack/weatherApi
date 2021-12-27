3e767083c4b600085bed24253d881256d970b6f4
RoySamson-stack <rsam9009@gmail.com>
2021-12-27 21:34:28 +0300

    adding the query forto show the country according to what the user has entered

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.post("/", function(req, res){
       const icon = weatherData.weather[0].icon
       const imageUrl = "http://openweathermap.org/img/wn/"+ icon +"@2x.png"
       res.write("<p>The weather is currently " + weatherDescription + "</p>")
-      res.write("<h1>The temperature in London is " + temp + " degree celsius </h1>");
+      res.write("<h1>The temperature in " +  query +" is " + temp + " degree celsius </h1>");
       res.write("<img src='" + imageUrl + "'>")
       res.send()
       // const object = {
